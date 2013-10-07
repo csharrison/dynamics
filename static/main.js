9ddfc566a7d758ab650229554010d60d33d3f89f
Charlie Harrison <charlieisharrison@gmail.com>
2013-10-07 01:47:40 -0400

    3d working better

--- static/main.js
+++ static/main.js
@@ -1,14 +1,17 @@
-var dxdt,dydt, dzdt, WIDTH, HEIGHT;
+var WIDTH = 20, HEIGHT = 20, DEPTH = 20;
 
+function rand(){
+	return (Math.random() * 2) - 1;
+}
+function is2D(){
+	return $("#dzdt").val() === "0";
+}
 var ymin = -20,
 	ymax = 20,
 	xmin = -20,
 	xmax = 20,
 	zmin = -20,
-	zmax = 20,
-	step = .1,
-	tend = 15,
-	t0 = 0;
+	zmax = 20;
 function getColorString(z){
 	z = Math.pow(z,2);
 	var r = ((z % 255)+255) % 255;;
@@ -16,94 +19,83 @@ function getColorString(z){
 	console.log(z);
 	return s;
 }
-function screenToXY(x,y){
-	var sx = (x/WIDTH) * (xmax - xmin) + xmin;
-	var sy = ((HEIGHT - y)/HEIGHT) * (ymax - ymin) + ymin;
-	return [sx, sy];
+function evalArguments(){
+	var dxdt, dydt, dzdt, step, t0, tend;
+	eval("dxdt = function(t,x,y,z){ return " + $("#dxdt").val() + "; }");
+	eval("dydt = function(t,x,y,z){ return " + $("#dydt").val() + "; }");
+	eval("dzdt = function(t,x,y,z){ return " + $("#dzdt").val() + "; }");
+	step = parseFloat($("#step").val(),10);
+	t0 = parseFloat($("#t0").val(),10);
+	tend = parseFloat($("#tend").val(),10);
+	y0 = _.map(['x0','y0','z0'], function(elt){
+		return parseFloat($("#"+elt).val(),10);
+	});
+	return [y0, dxdt, dydt, dzdt, step, t0, tend];
 }
-function XYToScreen(sx,sy){
-	var x = WIDTH * ((sx - xmin)/(xmax - xmin));
-	var y = HEIGHT * ((sy - ymin)/(ymax - ymin));
-	y = HEIGHT - y;
-	return [x,y];
+
+function reset(){
+	mathbox.remove("*");
+	return false;
+}
+
+function drawIt(y0, dxdt, dydt, dzdt, step, t0, tend){
+	var points = odeSolve(dxdt, dydt, dzdt, y0, step, t0, tend);
+	mathbox.grid()
+	if(! is2D()){ mathbox.grid({axis:[1,2]});}
+	mathbox/*.axis({
+	  id: 'x-axis',
+	  axis: 0,
+	  color: 0xa0a0a0,
+	  ticks: 5,
+	  lineWidth: 2,
+	  size: .05,
+	  labels: true,
+	})/*
+	.axis({
+	  id: 'y-axis',
+	  axis: 1,
+	  color: 0xa0a0a0,
+	  ticks: 5,
+	  lineWidth: 2,
+	  size: .05,
+	  labels: true,
+	  zero: false,
+	})
+	.axis({
+	  id: 'z-axis',
+	  axis: 2,
+	  color: 0xa0a0a0,
+	  ticks: 5,
+	  lineWidth: 2,
+	  size: .05,
+	  zero: false,
+	  labels: true,
+	})*/
+	.curve({
+	  n: points.length,
+	  id: Math.random().toString(),
+	  data: points,
+	  line: true,
+	  points: false,
+	  lineWidth: 1,
+	});
 }
 DomReady.ready(function(){
-	var $dxdt = $("#dxdt");
-	var $dydt = $("#dydt");
-	var $dzdt = $("#dzdt");
 	$("#functions").submit(function(){
-		eval("dxdt = function(t,x,y,z){ return " + $dxdt.val() + "; }");
-		eval("dydt = function(t,x,y,z){ return " + $dydt.val() + "; }");
-		eval("dzdt = function(t,x,y,z){ return " + $dzdt.val() + "; }");
-		step = parseFloat($("#step").val(),10);
-		t0 = parseFloat($("#t0").val(),10);
-		tend = parseFloat($("#tend").val(),10);
-
-
-		var points = odeSolve(dxdt, dydt, dzdt, [1,2,1] , step, t0, tend);
-
-		window.mathbox.curve({
-		  n: points.length,
-		  id: 'mine-circle',
-		  data: points,
-		  line: true,
-		  points: false,
-		  lineWidth: 1,
-		})
+		reset();
+		drawIt.apply(null, evalArguments());
 		return false;
-
-
 	})
 	$("#random").click(function(){
-		for(var i = 0; i < 100; i++){
-			//drawCurve(Math.random()*WIDTH, Math.random()*HEIGHT);
+		if(is2D()){ DEPTH = 0; }
+		for(var i = 0; i < 20; i++){
+			var args = evalArguments();
+			args[0] = [rand()*WIDTH, rand()*HEIGHT, rand() * DEPTH];
+			drawIt.apply(null, args);
 		}
+		return false;
 	});
 	$("#clear").click(reset);
 
-	//var canvas = $("canvas");
 
-	//window.context = canvas[0].getContext("2d");
-
-	function reset(){
-		var c = canvas[0];
-		var winheight = $(window).height()- 20;
-		HEIGHT = winheight;
-		WIDTH = winheight ;
-		c.height = HEIGHT;
-		c.width = WIDTH;
-	}
-	//window.onresize = reset;
-	//reset();
-	function drawCurve(ex, ey){
-		var p = screenToXY(ex, ey);
-		var x = p[0],
-			y = p[1],
-			z = 0;
-
-
-		context.beginPath()
-
-
-		var points = odeSolve(dxdt, dydt, dzdt, [x,y,z] , step, t0, tend);
-		context.moveTo(ex, ey);
-
-		for(var i = 0; i < points.length; i++){
-			var np = points[i];
-			x = np[0],
-			y = np[1];
-			z = np[2];
-
-			var sp = XYToScreen(x,y);
-			context.strokeStyle = getColorString(z);
-			console.log(context.strokeStyle)
-			context.lineTo(sp[0],sp[1]);
-			context.moveTo(sp[0],sp[1]);
-		}
-		context.stroke();
-	}
-
-	/*canvas.click(function(e){
-		drawCurve(e.offsetX, e.offsetY);
-	});*/
 });
